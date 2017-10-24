41df1606b6b9c30ad01d3ea359f50f888bfa7f32
Francieli Viane <francielifrv@gmail.com>
2017-10-24 23:36:41 +0200

    Returns http status if no transaction earlier than 60 seconds is found

--- src/main/java/com/statistics/service/StatisticService.java
+++ src/main/java/com/statistics/service/StatisticService.java
@@ -2,8 +2,11 @@ package com.statistics.service;
 
 import com.statistics.models.TransactionStatistics;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StatisticService {
@@ -15,7 +18,10 @@ public class StatisticService {
     }
 
     public ResponseEntity<TransactionStatistics> calculateStatisticsForTransactionsEarlierThanSixtySeconds() {
-        transactionService.retrieveTransactionEarlierThanSixtySeconds();
+        List<Double> transactionData = transactionService.retrieveTransactionEarlierThanSixtySeconds();
+        if (transactionData.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
         return null;
     }
--- src/test/java/com/statistics/service/StatisticServiceTest.java
+++ src/test/java/com/statistics/service/StatisticServiceTest.java
@@ -1,14 +1,23 @@
 package com.statistics.service;
 
+import com.statistics.models.TransactionStatistics;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+import java.util.Collections;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class StatisticServiceTest {
+
     @Mock
     TransactionService transactionService;
 
@@ -24,5 +33,16 @@ public class StatisticServiceTest {
         service.calculateStatisticsForTransactionsEarlierThanSixtySeconds();
 
         verify(transactionService, times(1)).retrieveTransactionEarlierThanSixtySeconds();
+    }
+
+    @Test
+    public void returnsHttpStatusNotFoundIfThereIsNoTransactionEarlierThanSixtySeconds() throws Exception {
+        when(transactionService.retrieveTransactionEarlierThanSixtySeconds()).thenReturn(Collections.emptyList());
+
+        StatisticService service = new StatisticService(transactionService);
+
+        ResponseEntity<TransactionStatistics> response = service.calculateStatisticsForTransactionsEarlierThanSixtySeconds();
+
+        assertThat(response.getStatusCode(), is(HttpStatus.NOT_FOUND));
     }
 }
